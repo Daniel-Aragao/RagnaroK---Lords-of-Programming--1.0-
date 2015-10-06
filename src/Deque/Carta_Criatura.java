2da381cbf58f4b108aad7bfdca9ad7c468c6d2d7
Daniel Aragão <dan.dan.44@hotmail.com>
2015-10-06 11:40:49 -0300

    Conclusão método importar

--- src/Deque/Carta_Criatura.java
+++ src/Deque/Carta_Criatura.java
@@ -7,6 +7,9 @@ public class Carta_Criatura extends Carta {
 		private int ataque;
 		private int defesa;
 		private int skill;
+		private int valor_do_modo;
+		private boolean modo; //true-ataque false-defesa
+		private boolean tab_state; //true-on deck false-on tab
 		
 	// Carta Mágica Associada
 		private Carta_Magica magica ;
@@ -16,13 +19,19 @@ public class Carta_Criatura extends Carta {
 		
 	public Carta_Criatura(String nome,String descricao, int ataque, int defesa, int skill,
 			BufferedImage imagem) {
-		super(nome, descricao, imagem);
+		super(nome, descricao, imagem, Tipo_Carta.CRIATURA);
 		// TODO Auto-generated constructor stub
 		this.ataque = ataque;
 		this.defesa = defesa;
 		this.skill = skill;
 		this.magica = null;
+		this.modo = true;
+		this.tab_state = true;
 	}
+	public boolean getModo(){return modo;}
+	private void ChangeModo(){ modo = !modo;}
+	public boolean gettab_state(){ return tab_state;}
+	public void changeTab_state(){ tab_state = !tab_state;}
 	
 	public int getAtaque() {
 		return ataque;
